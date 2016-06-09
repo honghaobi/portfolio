63145c7204010696ed9a7367b690923a15a4264c
henrybi <henrybi@u.washington.edu>
2016-06-09 12:51:37 -0700

    work page squares version 1

---
+++ js/page.js
@@ -0,0 +1,24 @@
+showHomePage();
+
+$('#show-buttons div').on('click', function(event){
+  $('#cube').attr('class',$(this).attr('class'));
+
+  if ($('#cube').attr('class')==='show-bottom') {
+    showHomePage();
+  } else if ($('#cube').attr('class')==='show-left') {
+    showWorkPage();
+  } else if ($('#cube').attr('class')==='show-top') {
+    showAboutPage();
+  } else if ($('#cube').attr('class')==='show-back') {
+    showSkillPage();
+  } else if ($('#cube').attr('class')==='show-front') {
+    showContactPage();
+  }
+});
+function showHomePage() {
+  $('.work-page').hide();
+}
+
+function showWorkPage() {
+  $('.work-page').show();
+}
