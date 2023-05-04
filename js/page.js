f197eb63e64540e0a89256ff95f866dafdbea3a0
henrybi <henrybidesign@gmail.com>
2023-05-04 10:23:09 -0700

    Add placeholder projects

--- js/page.js
+++ js/page.js
@@ -1,17 +1,27 @@
 var G = {
   rotating: false,
-  rotateNum: {x:0, y:0, z:0}
+  rotateNum: {
+    x: 0,
+    y: 0,
+    z: 0
+  }
 };
 
 function cubeRotate() {
-  Math.range = function (min, max) { return min + (Math.random() * (max - min));};
-  var randNumbX = Math.range(-0.01,0.01);
-  var randNumbY = Math.range(-0.01,0.01);
-  var randNumbZ = Math.range(-0.01,0.01);
+  Math.range = function(min, max) {
+    return min + (Math.random() * (max - min));
+  };
+  var randNumbX = Math.range(-0.01, 0.01);
+  var randNumbY = Math.range(-0.01, 0.01);
+  var randNumbZ = Math.range(-0.01, 0.01);
   G.rotating = true;
-  setTimeout(function () {
+  setTimeout(function() {
     G.rotating = false;
-    G.rotateNum = {x:randNumbX, y:randNumbY, z:randNumbZ};
+    G.rotateNum = {
+      x: randNumbX,
+      y: randNumbY,
+      z: randNumbZ
+    };
   }, 1000);
 }
 
@@ -21,48 +31,48 @@ $(window).load(function() {
   showHomePage();
 });
 
-$('#sidebar ul li').on('click', function(event){
-  $('#cube').attr('class',$(this).children('span').children('div').attr('class'));
-  if ($('#cube').attr('class')==='show-bottom') {
+$('#sidebar ul li').on('click', function(event) {
+  $('#cube').attr('class', $(this).children('span').children('div').attr('class'));
+  if ($('#cube').attr('class') === 'show-bottom') {
     showHomePage();
-  } else if ($('#cube').attr('class')==='show-left') {
+  } else if ($('#cube').attr('class') === 'show-left') {
     showWorkPage();
-  } else if ($('#cube').attr('class')==='show-top') {
+  } else if ($('#cube').attr('class') === 'show-top') {
     showAboutPage();
-  } else if ($('#cube').attr('class')==='show-back') {
+  } else if ($('#cube').attr('class') === 'show-back') {
     showSkillPage();
-  } else if ($('#cube').attr('class')==='show-right') {
+  } else if ($('#cube').attr('class') === 'show-right') {
     showContactPage();
   }
 });
 
 $('#cube .bottom').on('click', function(event) {
-  $('#cube').attr('class','show-left');
+  $('#cube').attr('class', 'show-left');
   showWorkPage();
 });
 
 $('#cube .left').on('click', function(event) {
-  $('#cube').attr('class','show-top');
+  $('#cube').attr('class', 'show-top');
   showAboutPage();
 });
 
 $('#cube .top').on('click', function(event) {
-  $('#cube').attr('class','show-back');
+  $('#cube').attr('class', 'show-back');
   showSkillPage();
 });
 
 $('#cube .back').on('click', function(event) {
-  $('#cube').attr('class','show-right');
+  $('#cube').attr('class', 'show-right');
   showContactPage();
 });
 
 $('#cube .right').on('click', function(event) {
-  $('#cube').attr('class','show-front');
+  $('#cube').attr('class', 'show-front');
   showHomePage();
 });
 
 $('#cube .front').on('click', function(event) {
-  $('#cube').attr('class','show-bottom');
+  $('#cube').attr('class', 'show-bottom');
   showHomePage();
 });
 
@@ -73,11 +83,11 @@ function showHomePage() {
 
 function showWorkPage() {
   cubeRotate();
-  $('.about-page, .skills-page, .contact-page, .up-show, .epac-show, .ymsw-show').hide();
+  $('.about-page, .skills-page, .contact-page, .cv-show, .ripld-show, riplw-show, .up-show, .wedding-show, .epac-show, .ymsw-show').hide();
   $('.work-page').show();
-  $('.work-page-menu ul li').siblings().removeClass().first().addClass('up-title');
-  $('.up').siblings().hide();
-  $('.up').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
+  $('.work-page-menu ul li').siblings().removeClass().first().addClass('cv-title');
+  $('.cv').siblings().hide();
+  $('.cv').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
   $('.work-page-menu').addClass('animated fadeInUp');
 }
 
@@ -116,7 +126,23 @@ $('.work-page-menu ul li').on('click', function(event) {
   var selected = $(this);
 
   selected.siblings().removeClass();
-  if (selected.html() === 'UP UP &amp; AWAY') {
+  if (selected.html() === 'CLEARVOICE') {
+    selected.addClass('cv-title');
+    $('.cv').siblings().removeClass('fadeInDown').addClass('fadeOutDown');
+    $('.cv').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
+  } else if (selected.html() === 'RIPL DESIGN') {
+    selected.addClass('ripld-title');
+    $('.ripld').siblings().removeClass('fadeInDown').addClass('fadeOutDown');
+    $('.ripld').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
+  } else if (selected.html() === 'RIPL WEB APP') {
+    selected.addClass('riplw-title');
+    $('.riplw').siblings().removeClass('fadeInDown').addClass('fadeOutDown');
+    $('.riplw').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
+  } else if (selected.html() === 'WEDDING') {
+    selected.addClass('wedding-title');
+    $('.wedding').siblings().removeClass('fadeInDown').addClass('fadeOutDown');
+    $('.wedding').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
+  } else if (selected.html() === 'UP UP &amp; AWAY') {
     selected.addClass('up-title');
     $('.up').siblings().removeClass('fadeInDown').addClass('fadeOutDown');
     $('.up').removeClass('fadeOutDown').addClass('animated fadeInDown').show();
@@ -132,9 +158,43 @@ $('.work-page-menu ul li').on('click', function(event) {
 });
 
 $('.arrow').on('click', function(event) {
-  $('.work-page').children().animate({ scrollTop: 0 }, 'slow');
+  $('.work-page').children().animate({
+    scrollTop: 0
+  }, 'slow');
 });
 
+$('.cv').on('click', function(event) {
+  $('.cv-show').removeClass('animated slideOutLeft').addClass('animated slideInRight').show();
+  $('.cv-close, .arrow').insertBefore('.work-page').addClass('animated slideInUp');
+});
+$('.cv-close').on('click', function(event) {
+  $('.cv-show').removeClass('animated slideInRight').addClass('animated slideOutLeft');
+  $('.cv-close, .arrow').appendTo('.up-show');
+});
+$('.ripld').on('click', function(event) {
+  $('.ripld-show').removeClass('animated slideOutLeft').addClass('animated slideInRight').show();
+  $('.ripld-close, .arrow').insertBefore('.work-page').addClass('animated slideInUp');
+});
+$('.ripld-close').on('click', function(event) {
+  $('.ripld-show').removeClass('animated slideInRight').addClass('animated slideOutLeft');
+  $('.ripld-close, .arrow').appendTo('.up-show');
+});
+$('.riplw').on('click', function(event) {
+  $('.riplw-show').removeClass('animated slideOutLeft').addClass('animated slideInRight').show();
+  $('.riplw-close, .arrow').insertBefore('.work-page').addClass('animated slideInUp');
+});
+$('.riplw-close').on('click', function(event) {
+  $('.riplw-show').removeClass('animated slideInRight').addClass('animated slideOutLeft');
+  $('.riplw-close, .arrow').appendTo('.up-show');
+});
+$('.wedding').on('click', function(event) {
+  $('.wedding-show').removeClass('animated slideOutLeft').addClass('animated slideInRight').show();
+  $('.wedding-close, .arrow').insertBefore('.work-page').addClass('animated slideInUp');
+});
+$('.wedding-close').on('click', function(event) {
+  $('.wedding-show').removeClass('animated slideInRight').addClass('animated slideOutLeft');
+  $('.wedding-close, .arrow').appendTo('.up-show');
+});
 $('.up').on('click', function(event) {
   $('.up-show').removeClass('animated slideOutLeft').addClass('animated slideInRight').show();
   $('.up-close, .arrow').insertBefore('.work-page').addClass('animated slideInUp');
